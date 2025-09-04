5d8367d9451231d05d37f51cd5d57280745cf8cc
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-09-04 15:59:14 +0530

    fib

--- lecture6/fibonacci.cpp
+++ lecture6/fibonacci.cpp
@@ -1,4 +1,16 @@
 #include<stdio.h>
 int main(){
-    int a;
+    int n;
+    printf("enter no of terms:");
+    scanf("%d",&n);
+    int t1=0;
+    int t2=1;
+    int next;
+    for (int i=1;i<=n;i++){
+        printf("%d ",t1);
+        next= t1 + t2;
+        t1=t2;
+        t2=next;
+    }
+    return 0;
 }
