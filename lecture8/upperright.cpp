f3c5f919fee38f44d4db9839f22cba1c38fae32f
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-09-08 16:02:50 +0530

    patterns

---
+++ lecture8/upperright.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main(){
+    int n;
+    printf("input no of rows=");
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        //space
+        for (int j=0;j<=i;j++){
+            printf(" ");
+        }
+        //star
+        for(int j=0;j<n-i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+ }
