167d05ce48f2907baecb986c9bed5c9cc6feb380
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-09-15 15:57:48 +0530

    magic no

--- lecture11/magicnousingfn.cpp
+++ lecture11/magicnousingfn.cpp
@@ -1,26 +1,36 @@
 #include<stdio.h>
-int magicno_check(int n){
-  int sum;
-    if(n>9){
-      sum=0;
+int digits_addition(int n){
+      int sum=0;
       while(n>0){
         int last_digit=n%10;
         sum=sum+last_digit;
         n=n/10;
       }
-      n=sum;
-    }
-    return 0;
-    }
+      return sum;
+}
+int magicno_check(int n){
+  int result=n;
+      while(result>9){
+        result= digits_addition(result);
+      }
+      if(result==1)
+        return 1;
+        else
+        return 0;
+     
+      
+}
+    
+    
 
 
 int main(){
-      int num=1234;
+      int num=1548;
       int n=num;
-      if(magicno_check(n)==1){
+      if(magicno_check(n)){
         printf("%d is a magic no",num);
       }else{
         printf("%d is not a magic no",num);
       }
       return 0;
-}
+} 
