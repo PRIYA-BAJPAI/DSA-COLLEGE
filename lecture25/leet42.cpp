43536354ecb232a8312ee810568d2d351d3bc8e7
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-10-15 19:37:34 +0530

    leetcode 42

---
+++ lecture25/leet42.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={4,3,0,6,2};
+    int leftmax[5];
+    int rightmax[5];
+    for(int i=0;i<5;i++){
+        if(arr[i]>arr[i-1]){
+            leftmax[i]=arr[i];
+        }else{
+            leftmax[i]=leftmax[i-1];
+        }
+        if(arr[i]>arr[i+1]){
+            rightmax[i]=arr[i];
+        }else{
+            leftmax[i]=leftmax[i-1];
+        }
+    }
+    int ans=0;
+    for(int i=0;i<5;i++){
+        int min=0;
+        if(leftmax[i]>rightmax[i]){
+            min=rightmax[i];
+        }else{
+            min=leftmax[i];
+        }
+        ans=ans+(min-arr[i]);
+    }
+    printf("%d",ans);
+    return 0;
+    
+}
