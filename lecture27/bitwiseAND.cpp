6ac871c74d5c22d1c9273186aaebbb823fbe78e6
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-10-28 15:53:07 +0530

    bitwise

---
+++ lecture27/bitwiseAND.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter number : ");
+    scanf("%d",&n);
+    if(n&1){
+        printf("Odd");
+    }else{
+        printf("Even");
+    }
+    return 0;
+}
